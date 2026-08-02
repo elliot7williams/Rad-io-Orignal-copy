8654c45bac5c2570be03174918429bca71d8413d
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:32:01 -0400

    Add Rad-io copy/Rad-io/Rad_ioApp.swift

---
+++ Rad-io copy/Rad-io/Rad_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rad_ioApp.swift
+//  Rad-io
+//
+//  Created by Elliot Williams on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Rad_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RadioView()
+        }
+    }
+}
