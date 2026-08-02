e6305da20fc0106234d3a5f01df2fe23dd411c0e
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:32:03 -0400

    Add Rad-io copy/Rad-ioTests/Rad_ioTests.swift

---
+++ Rad-io copy/Rad-ioTests/Rad_ioTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rad_ioTests.swift
+//  Rad-ioTests
+//
+//  Created by Elliot Williams on 2025-05-31.
+//
+
+import Testing
+@testable import Rad_io
+
+struct Rad_ioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
